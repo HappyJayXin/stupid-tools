2f4c50e0827850da778927173f4cbb06ad0d9042
Jay <xingyu.chen@kdanmobile.com>
2020-11-04 10:08:58 +0800

    fix not keep email

--- scripts/10min/index.js
+++ scripts/10min/index.js
@@ -2,15 +2,14 @@ const puppeteer = require('puppeteer-core');
 
 (async () => {
   const browser = await puppeteer.launch({
-    executablePath:
-			'/Applications/Brave Browser.app/Contents/MacOS/Brave Browser',
-    headless: false
+    executablePath: '/Applications/Brave Browser.app/Contents/MacOS/Brave Browser',
+    headless: false,
   });
   const page = await browser.newPage();
   await page.goto('https://10minutemail.net/');
-	await page.click('#copy-button');
-	while (true) {
-		await page.waitFor(1000 * 60 * 10 - 1000);
-		await page.click('a[href*="more"]');
-	}
+  await page.click('#copy-button');
+  while (true) {
+    await page.waitFor(1000 * 60 * 10 - 10000);
+    await page.click('a[href*="more"]');
+  }
 })();
